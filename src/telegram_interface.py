3bde62b9f1c23f004ef621c03f8bd020d6a90cf9
Andrei Protsenko <prondy@gmail.com>
2023-04-10 03:46:48 +0200

    fix async issue

--- src/telegram_interface.py
+++ src/telegram_interface.py
@@ -19,7 +19,7 @@ class TelegramBot:
         self.token = token
         self.chat_server = chat_server
         self.app = ApplicationBuilder().token(token).build()
-        self.chat_session = None
+        self.chat_sessions = {}
 
         start_handler = CommandHandler('start', self.start)
         reset_handler = CommandHandler('reset', self.reset)
@@ -37,23 +37,31 @@ class TelegramBot:
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         tg_chat_id = update.effective_chat.id
-        self.chat_session = await self.chat_server.create_chat(chat_id=tg_chat_id)
+        chat_session = await self.chat_server.create_chat(chat_id=tg_chat_id)
+        self.chat_sessions[tg_chat_id] = chat_session
         await context.bot.send_message(chat_id=update.effective_chat.id, text=TG_START_MESSAGE, parse_mode="Markdown")
         logger.info(f"Start command received: chatid: {update.message.chat_id}")
 
     async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        self.chat_session.reset_messages()
+        tg_chat_id = update.effective_chat.id
+        chat_session = self.chat_sessions.get(tg_chat_id)
+        if chat_session is None:
+            await self.start(update=update, context=context)
+            return
+        await chat_session.reset_messages()
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text="Весь контекст удален, начинаем новый чат с чистого листа")
         logger.info(f"Reset command received: chatid: {update.message.chat_id}")
 
     async def get_answer(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        tg_chat_id = update.effective_chat.id
+        chat_session = self.chat_sessions.get(tg_chat_id)
         # if for some reason we received a message without command /start call /start intentionally
-        if self.chat_session is None:
+        if chat_session is None:
             await self.start(update=update, context=context)
             return
         logger.info(f"Message received: chatid: {update.message.chat_id}, message: {update.message.text}")
-        response = await self.chat_session.new_message(update.message.text)
+        response = await chat_session.new_message(update.message.text)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=response, parse_mode='Markdown')
         logger.info(f"Answer received: chatid: {update.message.chat_id}, message: {response}")
 
